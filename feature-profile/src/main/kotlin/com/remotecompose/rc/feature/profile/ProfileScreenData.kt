e510ba66682cddfb49d2592bd1d697691339f4a3
rohitjar <rohitkumar@changejar.in>
2026-07-06 16:26:42 +0530

    Refactor

--- feature-profile/src/main/kotlin/com/remotecompose/rc/feature/profile/ProfileScreenData.kt
+++ feature-profile/src/main/kotlin/com/remotecompose/rc/feature/profile/ProfileScreenData.kt
@@ -2,10 +2,10 @@ package com.remotecompose.rc.feature.profile
 
 data class ProfileScreenData(
     val title: String = "Profile",
-    val userName: String = "Rohit Kumar",
-    val userInitials: String = "AS",
-    val phoneNumber: String = "+91-9305249853",
-    val age: String = "28",
+    val userName: String = "Sheen Kumar",
+    val userInitials: String = "RK",
+    val phoneNumber: String = "+91-9999911111",
+    val age: String = "23",
     val gender: String = "Male",
     val kycVerified: Boolean = true,
     val primaryUpiId: String = "9999999999@ybl",
